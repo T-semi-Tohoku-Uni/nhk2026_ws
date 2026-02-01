26a4465faed47eaf6142a463dbafc1614bb611a0
aky <akiyugata271@gmail.com>
2026-02-02 01:33:40 +0900

    nhk2026_canbridge.hppにCANブリッジクラスの定義を追加し、ライフサイクルノードのコールバックメソッドを実装しました

--- src/nhk2026_bridge/include/nhk2026_bridge/nhk2026_canbridge.hpp
+++ src/nhk2026_bridge/include/nhk2026_bridge/nhk2026_canbridge.hpp
@@ -8,3 +8,27 @@
 
 #include "std_msgs/msg/float32_multi_array.hpp"
 #include "std_msgs/msg/int32_multi_array.hpp"
+#include "std_msgs/msg/byte_multi_array.hpp"
+
+class CanBridgenhk2026
+: public rclcpp_lifecycle::LifecycleNode
+{
+public:
+    CanBridgenhk2026();
+    using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+private:
+    const char* Ifname;
+
+    CallbackReturn on_configure(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_activate(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_deactivate(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_cleanup(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_error(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_shutdown(const rclcpp_lifecycle::State &state);
+
+    OnSetParametersCallbackHandle::SharedPtr parameter_callback_handle_;
+    rcl_interfaces::msg::SetParametersResult parameters_callback(
+        const std::vector<rclcpp::Parameter> &parameters
+    );
+};
