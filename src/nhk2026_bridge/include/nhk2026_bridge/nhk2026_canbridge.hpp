34467ce22eacb070640a1c882b0a5e95f85b96c9
aky <akiyugata271@gmail.com>
2026-02-02 01:29:00 +0900

    nhk2026_canbridge.cppファイルを新規作成し、メイン関数を実装しました

--- src/nhk2026_bridge/include/nhk2026_bridge/nhk2026_canbridge.hpp
+++ src/nhk2026_bridge/include/nhk2026_bridge/nhk2026_canbridge.hpp
@@ -1,2 +1,10 @@
 #pragma once
 
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "rclcpp_lifecycle/lifecycle_publisher.hpp"
+
+#include "ros2can_bridge.hpp"
+
+#include "std_msgs/msg/float32_multi_array.hpp"
+#include "std_msgs/msg/int32_multi_array.hpp"
