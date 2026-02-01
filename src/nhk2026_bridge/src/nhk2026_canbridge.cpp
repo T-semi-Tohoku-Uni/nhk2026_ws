5611642d0cb0c9dcf281750c416767729a7e6e05
aky <akiyugata271@gmail.com>
2026-02-02 01:56:38 +0900

    CanBridgenhk2026クラスにライフサイクルコールバックメソッドを実装し、メイン関数でノードを初期化してスピンする処理を追加

--- src/nhk2026_bridge/src/nhk2026_canbridge.cpp
+++ src/nhk2026_bridge/src/nhk2026_canbridge.cpp
@@ -1,9 +1,95 @@
 #include "nhk2026_canbridge.hpp"
 
+#include <functional>
+
+using std::placeholders::_1;
+
+CanBridgenhk2026::CanBridgenhk2026()
+: rclcpp_lifecycle::LifecycleNode(std::string("nhk2026_canbridge"))
+{
+    
+}
+
+CanBridgenhk2026::CallbackReturn CanBridgenhk2026::on_configure(const rclcpp_lifecycle::State &state)
+{
+    RCLCPP_INFO(
+        get_logger(),
+        "on_configure() called. state: id=%u, label=%s",
+        state.id(),
+        state.label().c_str());
+    return CallbackReturn::SUCCESS;
+}
+
+CanBridgenhk2026::CallbackReturn CanBridgenhk2026::on_activate(const rclcpp_lifecycle::State &state)
+{
+    RCLCPP_INFO(
+        get_logger(),
+        "on_activate() called. state: id=%u, label=%s",
+        state.id(),
+        state.label().c_str());
+    return CallbackReturn::SUCCESS;
+}
+
+CanBridgenhk2026::CallbackReturn CanBridgenhk2026::on_deactivate(const rclcpp_lifecycle::State &state)
+{
+    RCLCPP_INFO(
+        get_logger(),
+        "on_deactivate() called. state: id=%u, label=%s",
+        state.id(),
+        state.label().c_str());
+    return CallbackReturn::SUCCESS;
+}
+
+CanBridgenhk2026::CallbackReturn CanBridgenhk2026::on_cleanup(const rclcpp_lifecycle::State &state)
+{
+    RCLCPP_INFO(
+        get_logger(),
+        "on_cleanup() called. state: id=%u, label=%s",
+        state.id(),
+        state.label().c_str());
+    return CallbackReturn::SUCCESS;
+}
+
+CanBridgenhk2026::CallbackReturn CanBridgenhk2026::on_error(const rclcpp_lifecycle::State &state)
+{
+    RCLCPP_INFO(
+        get_logger(),
+        "on_error() called. state: id=%u, label=%s",
+        state.id(),
+        state.label().c_str());
+    return CallbackReturn::SUCCESS;
+}
+
+CanBridgenhk2026::CallbackReturn CanBridgenhk2026::on_shutdown(const rclcpp_lifecycle::State &state)
+{
+    RCLCPP_INFO(
+        get_logger(),
+        "on_shutdown() called. state: id=%u, label=%s",
+        state.id(),
+        state.label().c_str());
+    return CallbackReturn::SUCCESS;
+}
+
+rcl_interfaces::msg::SetParametersResult CanBridgenhk2026::parameters_callback(
+    const std::vector<rclcpp::Parameter> &parameters)
+{
+    rcl_interfaces::msg::SetParametersResult result;
+    result.successful = true;
+    result.reason = "success";
+
+    for (const auto &param : parameters)
+    {
+        
+    }
+
+    return result;
+}
+
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    
+    std::shared_ptr<CanBridgenhk2026> node = std::make_shared<CanBridgenhk2026>();
+    rclcpp::spin(node->get_node_base_interface());
     rclcpp::shutdown();
     return 0;
 }
