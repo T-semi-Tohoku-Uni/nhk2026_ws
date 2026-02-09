f9ac0893aa9ba94e83acdb1ba0af5b4250aee75d
grafen1202 <grafen1202>
2026-02-09 18:19:54 +0900

    mcl lidar select add

--- src/nhk2026_core/launch/cpu_sim.launch.py
+++ src/nhk2026_core/launch/cpu_sim.launch.py
@@ -5,6 +5,8 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+from launch.actions import SetEnvironmentVariable
 from launch_ros.actions import Node
 import launch_ros
 
@@ -235,6 +237,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         launch_ros.actions.SetParameter(name='use_sim_time', value=True),
+        SetEnvironmentVariable(name='WITH_lidar', value='1'),
         gazebo,
         node_robot_state_publisher,
         gz_spawn_entity,
