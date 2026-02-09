63302eea70d160349816f439dfcbd4ce254fbd78
grafen1202 <grafen1202>
2026-02-09 21:35:18 +0900

    sim move

--- src/nhk2026_core/launch/nhk_sim.launch.py
+++ src/nhk2026_core/launch/nhk_sim.launch.py
@@ -35,7 +35,7 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
-        launch_arguments=[('gz_args', [f' -r  {world}'])]
+        launch_arguments=[('gz_args', [f' -r -s {world}'])]
     )
 
     xacro_file = os.path.join(package_dir, "urdf", "r2_robot.xacro")
