eadf98c69a04c8696ca1c7cb7250a9f40372451c
code <code@t-semi.org>
2026-02-10 11:19:22 +0900

    r1_1st_video.launch.pyを新規作成し、joy_vel_converterノードのライフサイクル管理を追加

---
+++ src/nhk2026_core/launch/r1_1st_video.launch.py
@@ -0,0 +1,57 @@
+from launch import LaunchDescription
+from launch_ros.actions import LifecycleNode
+from launch_ros.actions import Node
+
+from launch.actions import EmitEvent, RegisterEventHandler
+from launch.actions import OpaqueFunction, Shutdown
+from launch.event_handlers import OnProcessStart
+from launch_ros.event_handlers import OnStateTransition
+from launch_ros.events.lifecycle import ChangeState
+import lifecycle_msgs.msg
+import launch
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    name_space = 'r1'
+
+    vel_joy_node = LifecycleNode(
+        package='nhk2026_bridge',
+        executable='joy_vel_converter',
+        name='joy_vel_converter',
+        namespace=name_space,
+        output='screen',
+        emulate_tty=True,
+    )
+
+    ld.add_action(vel_joy_node)
+
+    vel_joy_configure_event_handler = RegisterEventHandler(
+        OnProcessStart(
+            target_action=vel_joy_node,
+            on_start=[
+                EmitEvent(
+                    event=ChangeState(
+                        lifecycle_node_matcher=launch.events.matches_action(vel_joy_node),
+                        transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
+                    )
+                )
+            ]
+    ))
+    ld.add_action(vel_joy_configure_event_handler)
+
+    vel_joy_activate_event_handler = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=vel_joy_node,
+            start_state='configuring',
+            goal_state='inactive',
+            entities=[
+                EmitEvent(
+                    event=ChangeState(
+                        lifecycle_node_matcher=launch.events.matches_action(vel_joy_node),
+                        transition_id=lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE,
+                    )
+                )
+            ]
+    ))
+    ld.add_action(vel_joy_activate_event_handler)
+    return ld
