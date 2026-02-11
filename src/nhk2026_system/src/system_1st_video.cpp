324308377d86f57fbb8fae3b782baa2d6fa75f7d
aky <akiyugata271@gmail.com>
2026-02-11 17:25:13 +0900

    System1stVideoクラスのインスタンス生成を修正し、メイン関数でのノードスピン処理を追加

--- src/nhk2026_system/src/system_1st_video.cpp
+++ src/nhk2026_system/src/system_1st_video.cpp
@@ -13,8 +13,8 @@ System1stVideo::System1stVideo()
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    // std::shared_ptr<CanBridgenhk2026> node = std::make_shared<CanBridgenhk2026>();
-    // rclcpp::spin(node->get_node_base_interface());
+    std::shared_ptr<System1stVideo> node = std::make_shared<System1stVideo>();
+    rclcpp::spin(node->get_node_base_interface());
     rclcpp::shutdown();
     return 0;
 }
