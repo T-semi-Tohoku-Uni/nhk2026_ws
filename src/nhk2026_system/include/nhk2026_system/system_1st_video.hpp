4c3251e0574ae870180478e5b98ae125d2089aa9
aky <akiyugata271@gmail.com>
2026-02-11 16:52:11 +0900

    System1stVideoクラスを追加し、ライフサイクルノードのコールバックメソッドを定義

--- src/nhk2026_system/include/nhk2026_system/system_1st_video.hpp
+++ src/nhk2026_system/include/nhk2026_system/system_1st_video.hpp
@@ -4,3 +4,17 @@
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "rclcpp_lifecycle/lifecycle_publisher.hpp"
 
+class System1stVideo
+: public rclcpp_lifecycle::LifecycleNode
+{
+public:
+    System1stVideo();
+    using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+private:
+    CallbackReturn on_configure(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_activate(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_deactivate(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_cleanup(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_error(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_shutdown(const rclcpp_lifecycle::State &state);
+};
