53fa2824a8ca6162b44cdc13a5e0de628f9c9434
aky <akiyugata271@gmail.com>
2026-02-11 17:24:41 +0900

    System1stVideoクラスのコンストラクタを実装し、パラメータコールバックを追加

--- src/nhk2026_system/src/system_1st_video.cpp
+++ src/nhk2026_system/src/system_1st_video.cpp
@@ -1,5 +1,15 @@
 #include "system_1st_video.hpp"
 
+using std::placeholders::_1;
+
+System1stVideo::System1stVideo()
+: rclcpp_lifecycle::LifecycleNode(std::string("syste,_1st_video"))
+{
+    this->parameter_callback_handle_ = this->add_on_set_parameters_callback(
+        std::bind(&System1stVideo::parameters_callback, this, _1)
+    );
+}
+
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
