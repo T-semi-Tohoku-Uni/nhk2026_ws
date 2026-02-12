6e8364e7d9a75962c7f4e4d590b39770677cf8a7
grafen1202 <grafen1202>
2026-02-12 09:27:04 +0900

    launch change

--- src/nhk2026_core/launch/nhk_R2_two.launch.py
+++ src/nhk2026_core/launch/nhk_R2_two.launch.py
@@ -160,6 +160,7 @@ def generate_launch_description():
         # joy_node,
         joy2Vel_node,
         urg_node_front,
+        urg_node_rear,
         # static_from_odom_to_basefootprint,
         vel_feedback_pass_through,
     ])
