dd21ea38930713f91caa48feb8cc04f5325983ea
aky <code@t-semi.org>
2026-02-24 07:12:40 +0000

    IdeArmActionServerクラスの定義を追加し、パラメータコールバックのメンバーを含める

--- src/nhk2026_system/include/nhk2026_system/ide_arm_action_server.hpp
+++ src/nhk2026_system/include/nhk2026_system/ide_arm_action_server.hpp
@@ -1,5 +1,19 @@
+#pragma once
+
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
 
 #include "nhk2026_msgs/action/arm_move.hpp"
 
+class IdeArmActionServer
+: public rclcpp::Node
+{
+public:
+    IdeArmActionServer();
+
+private:
+    OnSetParametersCallbackHandle::SharedPtr parameter_callback_handle_;
+    rcl_interfaces::msg::SetParametersResult parameters_callback(
+        const std::vector<rclcpp::Parameter> &parameters
+    );
+};
