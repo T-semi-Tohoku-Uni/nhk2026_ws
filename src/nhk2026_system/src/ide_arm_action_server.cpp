cd3dac0c81282115a8fe6bfaadede4ed62564180
aky <code@t-semi.org>
2026-02-25 05:36:41 +0000

    IdeArmActionServerクラスにパラメータコールバック、ゴール処理、キャンセル処理、受理処理のメソッドを追加

--- src/nhk2026_system/src/ide_arm_action_server.cpp
+++ src/nhk2026_system/src/ide_arm_action_server.cpp
@@ -6,6 +6,33 @@ IdeArmActionServer::IdeArmActionServer()
     
 }
 
+rcl_interfaces::msg::SetParametersResult IdeArmActionServer::parameters_callback(
+    const std::vector<rclcpp::Parameter> &parameters
+)
+{
+
+}
+
+rclcpp_action::GoalResponse IdeArmActionServer::handle_goal(
+    const rclcpp_action::GoalUUID &,
+    std::shared_ptr<const ArmMove::Goal> goal
+)
+{
+    
+}
+
+rclcpp_action::CancelResponse IdeArmActionServer::handle_cancel(
+    const std::shared_ptr<GoalHandleArmMove> goal_handle
+)
+{
+
+}
+
+void IdeArmActionServer::handle_accepted(const std::shared_ptr<GoalHandleArmMove> goal_handle)
+{
+
+}
+
 int main()
 {
 
