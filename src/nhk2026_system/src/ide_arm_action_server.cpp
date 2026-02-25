8d0d261d97ea2e1dd5c694728858876bbd776e14
aky <code@t-semi.org>
2026-02-25 05:47:30 +0000

    IdeArmActionServerクラスにアクションサーバーとパラメータコールバックを追加

--- src/nhk2026_system/src/ide_arm_action_server.cpp
+++ src/nhk2026_system/src/ide_arm_action_server.cpp
@@ -3,7 +3,17 @@
 IdeArmActionServer::IdeArmActionServer()
 : rclcpp::Node("ide_arm_action_server")
 {
-    
+    this->action_server_ = rclcpp_action::create_server<ArmMove>(
+        this,
+        "ide_arm",
+        std::bind(&IdeArmActionServer::handle_goal, this, _1, _2),
+        std::bind(&IdeArmActionServer::handle_cancel,this, _1),
+        std::bind(&IdeArmActionServer::handle_accepted, this, _1)
+    );
+
+    this->parameter_callback_handle_ = this->add_on_set_parameters_callback(
+        std::bind(&IdeArmActionServer::parameters_callback, this, _1)
+    );
 }
 
 rcl_interfaces::msg::SetParametersResult IdeArmActionServer::parameters_callback(
