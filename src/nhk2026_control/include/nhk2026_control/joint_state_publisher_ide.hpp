ba0e18c4c5099f29cdf031219bfbd7f6ccaeb107
aky <akiyugata271@gmail.com>
2026-02-27 00:03:16 +0900

    JointStatePublisherIdeクラスを追加し、コンストラクタとデストラクタを実装

---
+++ src/nhk2026_control/include/nhk2026_control/joint_state_publisher_ide.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/node.hpp"
+
+class JointStatePublisherIde
+: public rclcpp::Node
+{
+public:
+    JointStatePublisherIde();
+private:
+    /* data */
+};
---
+++ src/nhk2026_control/src/joint_state_publisher_ide.cpp
@@ -0,0 +1,11 @@
+#include "joint_state_publisher_ide.hpp"
+
+JointStatePublisherIde::JointStatePublisherIde()
+{
+
+}
+
+JointStatePublisherIde::~JointStatePublisherIde()
+{
+    
+}
