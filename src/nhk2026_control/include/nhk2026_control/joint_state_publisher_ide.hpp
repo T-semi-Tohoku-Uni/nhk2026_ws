fd338097a9ee03ad8f8a78eedeb1bf03381617b6
aky <code@t-semi.org>
2026-02-26 15:18:14 +0000

    JointStatePublisherIdeクラスに必要なヘッダーファイルを追加

--- src/nhk2026_control/include/nhk2026_control/joint_state_publisher_ide.hpp
+++ src/nhk2026_control/include/nhk2026_control/joint_state_publisher_ide.hpp
@@ -3,11 +3,14 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/node.hpp"
 
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "std_msgs/msg/float32_multi_array.hpp"
+
 class JointStatePublisherIde
 : public rclcpp::Node
 {
 public:
     JointStatePublisherIde();
 private:
-    /* data */
+    
 };
