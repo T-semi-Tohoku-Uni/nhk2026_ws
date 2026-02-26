910f46ea19dbb5fb3f36ea140fcfda8b896e61aa
aky <akiyugata271@gmail.com>
2026-02-27 00:10:23 +0900

    JointStatePublisherIdeクラスのデストラクタを削除し、コンストラクタを簡素化

--- src/nhk2026_control/src/joint_state_publisher_ide.cpp
+++ src/nhk2026_control/src/joint_state_publisher_ide.cpp
@@ -1,11 +1,7 @@
 #include "joint_state_publisher_ide.hpp"
 
 JointStatePublisherIde::JointStatePublisherIde()
-{
-
-}
-
-JointStatePublisherIde::~JointStatePublisherIde()
+: rclcpp::Node("joint_state_publisher_ide")
 {
 
 }
