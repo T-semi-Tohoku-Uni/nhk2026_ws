bfb78e30f6f463a085df0bc44f3f07bae60f1840
aky <code@t-semi.org>
2026-03-01 13:05:09 +0000

    base_linkをbase_link_r2に変更し、関連するメッシュファイルを追加

--- src/r2_step_climber_description/launch/robot_description.launch.py
+++ src/r2_step_climber_description/launch/robot_description.launch.py
@@ -32,4 +32,10 @@ def generate_launch_description():
             name='robot_state_publisher',
             output='screen',
             parameters=[robot_description]),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen',
+        )
     ])
