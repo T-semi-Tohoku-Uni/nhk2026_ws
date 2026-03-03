a5bd0210b8e503e9ab919f918f57ddcf12d0626f
grafen1202 <grafen1202>
2026-03-03 16:51:02 +0900

    daitai kaita

--- src/nhk2026_control/src/joint_state_publisher_dansa.cpp
+++ src/nhk2026_control/src/joint_state_publisher_dansa.cpp
@@ -1,29 +1,29 @@
-#include "nhk2026_control/joint_state_publisher_ide.hpp"
+#include "nhk2026_control/joint_state_publisher_dansa.hpp"
 
 using std::placeholders::_1;
 
-JointStatePublisherIde::JointStatePublisherIde()
-: rclcpp::Node("joint_state_publisher_ide")
+JointStatePublisherDansa::JointStatePublisherDansa()
+: rclcpp::Node("joint_state_publisher_dansa")
 {
     rclcpp::QoS device = rclcpp::QoS(rclcpp::KeepLast(10))
         .reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE)
         .durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
 
     this->joint_state_publisher_ = this->create_publisher<sensor_msgs::msg::JointState>(
-        "joint_states",
+        "joint_states_dansa",
         device
     );
     this->motor_feedback_subscriber_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
-        "motor_feedback",
+        "motor_dansa_feedback",
         device,
-        std::bind(&JointStatePublisherIde::motor_callback, this, _1)
+        std::bind(&JointStatePublisherDansa::motor_callback, this, _1)
     );
 }
 
-void JointStatePublisherIde::motor_callback(const std_msgs::msg::Float32MultiArray::SharedPtr rxdata)
+void JointStatePublisherDansa::motor_callback(const std_msgs::msg::Float32MultiArray::SharedPtr rxdata)
 {
     sensor_msgs::msg::JointState txdata;
-    txdata.name = {"joint1", "joint2", "joint3"};
+    txdata.name = {"right_arm", "left_arm", "back_arm"};
     txdata.position = {
         rxdata->data[0],
         rxdata->data[1],
@@ -37,7 +37,7 @@ void JointStatePublisherIde::motor_callback(const std_msgs::msg::Float32MultiArr
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    std::shared_ptr<JointStatePublisherIde> node = std::make_shared<JointStatePublisherIde>();
+    std::shared_ptr<JointStatePublisherDansa> node = std::make_shared<JointStatePublisherDansa>();
     rclcpp::spin(node->get_node_base_interface());
     rclcpp::shutdown();
     return 0;
---
+++ src/nhk2026_system/include/nhk2026_system/dansa_arm_action_server.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+
+#include "nhk2026_msgs/action/arm_move.hpp"
+#include <std_msgs/msg/float32_multi_array.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+
+#include <atomic>
+
+using namespace std::placeholders;
+
+class DansaArmActionServer
+: public rclcpp::Node
+{
+public:
+    DansaArmActionServer();
+
+private:
+    using ArmMove = nhk2026_msgs::action::ArmMove;
+    using GoalHandleArmMove = rclcpp_action::ServerGoalHandle<ArmMove>;
+    
+    OnSetParametersCallbackHandle::SharedPtr parameter_callback_handle_;
+    rcl_interfaces::msg::SetParametersResult parameters_callback(
+        const std::vector<rclcpp::Parameter> &parameters
+    );
+
+    rclcpp_action::Server<ArmMove>::SharedPtr action_server_;
+    rclcpp_action::GoalResponse handle_goal(
+        const rclcpp_action::GoalUUID &,
+        std::shared_ptr<const ArmMove::Goal> goal
+    );
+    rclcpp_action::CancelResponse handle_cancel(
+        const std::shared_ptr<GoalHandleArmMove> goal_handle
+    );
+    void handle_accepted(const std::shared_ptr<GoalHandleArmMove> goal_handle);
+    void execute(const std::shared_ptr<GoalHandleArmMove> goal_handle);
+
+    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr right_arm_motor_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr left_arm_motor_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr back_arm_motor_publisher_;
+
+    rclcpp::TimerBase::SharedPtr feedback_timer_;
+    void feedback_timer_callback();
+
+    std::atomic_bool goal_active_{false};
+    geometry_msgs::msg::PoseStamped goal_pos_;
+    geometry_msgs::msg::PoseStamped now_pos_;
+    sensor_msgs::msg::JointState now_joint_;
+
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_states_subscriber;
+    void joint_state_callback(const sensor_msgs::msg::JointState::SharedPtr rxdata);
+
+    float kPosTolerance_;
+    bool joint_subscribe_flag_{false};
+};
