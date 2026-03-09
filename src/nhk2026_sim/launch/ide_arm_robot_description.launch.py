e4def75713e6d185450fd37f68a134adc5a606ef
aky <code@t-semi.org>
2026-03-09 06:43:41 +0000

    update robot launch and xacro files, add arm_plane world and bridge node for simulation

--- src/nhk2026_sim/launch/ide_arm_robot_description.launch.py
+++ src/nhk2026_sim/launch/ide_arm_robot_description.launch.py
@@ -32,16 +32,16 @@ def generate_launch_description():
     )
     robot_description = {"robot_description": robot_description_content}
     package_dir = get_package_share_directory("nhk2026_sim")
-    x = 1.0
-    y = 1.0
-    z = 0.10
-    theta = math.pi/2
+    x = 0.0
+    y = 0.0
+    z = 0.0
+    theta = 0.0
     
     world = os.path.join(
-        get_package_share_directory("nhk2026_sim"), "worlds", "field_nhk.world"
+        get_package_share_directory("nhk2026_sim"), "worlds", "arm_plane.world"
     )
 
-    xacro_file = os.path.join(package_dir, "urdf", "r2_robot.xacro")
+    xacro_file = os.path.join(package_dir, "urdf", "ide_arm.xacro")
     doc = xacro.process_file(xacro_file, mappings={'use_sim' : 'true'})
     robot_desc = doc.toprettyxml(indent='  ')
     params = {'robot_description': robot_desc}
@@ -50,6 +50,26 @@ def generate_launch_description():
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
         launch_arguments=[('gz_args', [f' -r {world}'])]
+    )
+    
+    # Bridge
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='ros_gz_bridge_1',
+        arguments=[
+            '/joint1@std_msgs/msg/Float32@ignition.msgs.Float32',
+            '/joint2@std_msgs/msg/Float32@ignition.msgs.Float32',
+            '/joint3@std_msgs/msg/Float32@ignition.msgs.Float32',
+            '/tf@tf2_msgs/msg/TFMessage@ignition.msgs.Pose_V',
+            '/tf_static@tf2_msgs/msg/TFMessage@ignition.msgs.Pose_V',
+            '/world/nhk2026/clock@rosgraph_msgs/msg/Clock@ignition.msgs.Clock',
+            '/world/nhk2026/model/robot/joint_state@sensor_msgs/msg/JointState@ignition.msgs.Model'
+        ],
+        remappings=[
+            ('/world/nhk2026/model/robot/joint_state', '/joint_states'),
+        ],
+        output='screen'
     )
 
     return LaunchDescription([
