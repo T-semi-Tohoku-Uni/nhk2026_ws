c87ab86ec48841e7c6d03f92346c1f099e525b0a
aky <akiyugata271@gmail.com>
2026-03-09 13:28:31 +0900

    update robot launch and xacro files for simulation integra

--- src/nhk2026_sim/launch/ide_arm_robot_description.launch.py
+++ src/nhk2026_sim/launch/ide_arm_robot_description.launch.py
@@ -5,6 +5,13 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+import math
+import os
+import xacro
 
 def generate_launch_description():
 
@@ -24,6 +31,26 @@ def generate_launch_description():
         ]
     )
     robot_description = {"robot_description": robot_description_content}
+    package_dir = get_package_share_directory("nhk2026_sim")
+    x = 1.0
+    y = 1.0
+    z = 0.10
+    theta = math.pi/2
+    
+    world = os.path.join(
+        get_package_share_directory("nhk2026_sim"), "worlds", "field_nhk.world"
+    )
+
+    xacro_file = os.path.join(package_dir, "urdf", "r2_robot.xacro")
+    doc = xacro.process_file(xacro_file, mappings={'use_sim' : 'true'})
+    robot_desc = doc.toprettyxml(indent='  ')
+    params = {'robot_description': robot_desc}
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
+        launch_arguments=[('gz_args', [f' -r {world}'])]
+    )
 
     return LaunchDescription([
         Node(
@@ -33,4 +60,18 @@ def generate_launch_description():
             output='screen',
             parameters=[robot_description]
         ),
+        Node(
+            package='ros_gz_sim',
+            executable='create',
+            output='screen',
+            arguments=['-string', robot_desc,
+                    '-name', 'robot',
+                    '-allow_renaming', 'false',
+                    '-x', str(x),
+                    '-y', str(y),
+                    '-z', str(z),
+                    '-Y', str(theta)
+                    ],
+        ),
+        gazebo,
     ])
