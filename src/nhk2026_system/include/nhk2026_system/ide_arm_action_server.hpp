193aab7b5115c9f76a9d8425b032bbffc9beaba9
aky <code@t-semi.org>
2026-03-10 09:28:36 +0000

    fix: reorganize includes and update member variables in ide_arm_action_server.hpp

--- src/nhk2026_system/include/nhk2026_system/ide_arm_action_server.hpp
+++ src/nhk2026_system/include/nhk2026_system/ide_arm_action_server.hpp
@@ -4,9 +4,10 @@
 #include <rclcpp_action/rclcpp_action.hpp>
 
 #include "nhk2026_msgs/action/arm_move.hpp"
-#include <std_msgs/msg/float32_multi_array.hpp>
-#include <geometry_msgs/msg/pose_stamped.hpp>
-#include <sensor_msgs/msg/joint_state.hpp>
+#include "std_msgs/msg/float32_multi_array.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
 
 #include <kdl/tree.hpp>
 #include <kdl/chain.hpp>
@@ -31,13 +32,10 @@ public:
 private:
     using ArmMove = nhk2026_msgs::action::ArmMove;
     using GoalHandleArmMove = rclcpp_action::ServerGoalHandle<ArmMove>;
-    
-    OnSetParametersCallbackHandle::SharedPtr parameter_callback_handle_;
+
     rcl_interfaces::msg::SetParametersResult parameters_callback(
         const std::vector<rclcpp::Parameter> &parameters
     );
-
-    rclcpp_action::Server<ArmMove>::SharedPtr action_server_;
     rclcpp_action::GoalResponse handle_goal(
         const rclcpp_action::GoalUUID &,
         std::shared_ptr<const ArmMove::Goal> goal
@@ -47,22 +45,24 @@ private:
     );
     void handle_accepted(const std::shared_ptr<GoalHandleArmMove> goal_handle);
     void execute(const std::shared_ptr<GoalHandleArmMove> goal_handle);
+    void feedback_timer_callback();
+    void joint_state_callback(const sensor_msgs::msg::JointState::SharedPtr rxdata);
+    void robot_description_callback(const std_msgs::msg::String::SharedPtr rxdata);
 
+    OnSetParametersCallbackHandle::SharedPtr parameter_callback_handle_;
+    rclcpp_action::Server<ArmMove>::SharedPtr action_server_;
     rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr j1_motor_publisher_;
     rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr j2_motor_publisher_;
     rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr j3_motor_publisher_;
-
     rclcpp::TimerBase::SharedPtr feedback_timer_;
-    void feedback_timer_callback();
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_states_subscriber;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr robot_description_subscriber_;
 
     std::atomic_bool goal_active_{false};
+    float kPosTolerance_;
+    bool joint_subscribe_flag_{false};
+    bool robot_description_flag_{false};
     geometry_msgs::msg::PoseStamped goal_pos_;
     geometry_msgs::msg::PoseStamped now_pos_;
     sensor_msgs::msg::JointState now_joint_;
-
-    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_states_subscriber;
-    void joint_state_callback(const sensor_msgs::msg::JointState::SharedPtr rxdata);
-
-    float kPosTolerance_;
-    bool joint_subscribe_flag_{false};
 };
