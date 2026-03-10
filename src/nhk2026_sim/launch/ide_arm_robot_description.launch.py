789e991f871b629fe4979f006abb6081ddbefa16
aky <code@t-semi.org>
2026-03-10 08:43:09 +0000

    fix: update launch file to correct gazebo launch arguments for simulation

--- src/nhk2026_sim/launch/ide_arm_robot_description.launch.py
+++ src/nhk2026_sim/launch/ide_arm_robot_description.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
-        launch_arguments=[('gz_args', [f' -r {world}'])]
+        launch_arguments=[('gz_args', [f'-s -r {world}'])]
     )
     
     # Bridge
