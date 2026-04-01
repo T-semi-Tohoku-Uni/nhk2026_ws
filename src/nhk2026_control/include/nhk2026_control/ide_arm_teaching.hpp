f8d0024a269d54f4e976593385d778ddb58a07a2
aky <code@t-semi.org>
2026-04-01 10:29:42 +0000

    launch: Implement IdeArmTeaching class with transform listener and buffer

--- src/nhk2026_control/include/nhk2026_control/ide_arm_teaching.hpp
+++ src/nhk2026_control/include/nhk2026_control/ide_arm_teaching.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "rclcpp/rclcpp.hpp"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
+
+#include "geometry_msgs/msg/transform_stamped.hpp"
 
 class IdeArmTeaching
 : public rclcpp::Node
@@ -9,5 +13,8 @@ public:
     IdeArmTeaching();
 
 private:
-    
+    geometry_msgs::msg::TransformStamped listen_transform();
+
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
 };
--- src/nhk2026_control/src/ide_arm_teaching.cpp
+++ src/nhk2026_control/src/ide_arm_teaching.cpp
@@ -1,2 +1,13 @@
 #include "nhk2026_control/ide_arm_teaching.hpp"
 
+IdeArmTeaching::IdeArmTeaching()
+: rclcpp::Node("ide_arm_teaching")
+{
+    this->tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+    this->tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+}
+
+geometry_msgs::msg::TransformStamped IdeArmTeaching::listen_transform()
+{
+    
+}
