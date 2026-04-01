1b20807985e2b771ede14d6bd88a9ceaf98484c6
aky <code@t-semi.org>
2026-04-01 09:19:12 +0000

    launch: Add IdeArmTeaching class and implementation files

---
+++ src/nhk2026_control/include/nhk2026_control/ide_arm_teaching.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "rclcpp/rclcpp.hpp"
+
+class IdeArmTeaching
+: public rclcpp::Node
+{
+public:
+    IdeArmTeaching();
+
+private:
+    
+};
---
+++ src/nhk2026_control/src/ide_arm_teaching.cpp
@@ -0,0 +1,2 @@
+#include "nhk2026_control/ide_arm_teaching.hpp"
+
