78e21f1fe4d134418be53603a7ab1eb920d9cfc7
aky <code@t-semi.org>
2026-04-09 14:37:11 +0000

    video_2nd.launch.pyを修正; z座標の値を変更し、xacroファイルを更新、Gazeboブリッジと静的変換パブリッシャーを追加

--- src/nhk2026_core/launch/video_2nd.launch.py
+++ src/nhk2026_core/launch/video_2nd.launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
     ld = LaunchDescription()
     x = -1.47
     y = 0.45
-    z = 0.1
+    z = 0.2
     theta = 0.0
 
     sim_package_dir = get_package_share_directory("nhk2026_sim")
@@ -26,11 +26,11 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
-        launch_arguments=[('gz_args', [f' -r -s {world}'])]
+        launch_arguments=[('gz_args', [f' -r {world}'])]
     )
     ld.add_action(gazebo)
 
-    xacro_file = os.path.join(sim_package_dir, "urdf", "r2_2nd.xacro")
+    xacro_file = os.path.join(sim_package_dir, "urdf", "r2_all.xacro")
     doc = xacro.process_file(xacro_file, mappings={'use_sim' : 'true'})
     robot_desc = doc.toprettyxml(indent='  ')
     params = {'robot_description': robot_desc}
@@ -41,5 +41,46 @@ def generate_launch_description():
         parameters=[params]
     )
     ld.add_action(node_robot_state_publisher)
+
+    gz_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=[
+            '-string', robot_desc,
+            '-name', 'r2',
+            '-x', str(x),
+            '-y', str(y),
+            '-z', str(z),
+            '-Y', str(theta),
+        ]
+    )
+    ld.add_action(gz_spawn_entity)
+
     
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='ros_gz_bridge_1',
+        arguments=[
+            '/scan_front@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
+            '/scan_back@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
+            '/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry',
+            '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+            '/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V',
+            '/tf_static@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V',
+            '/world/nhk2026/clock@rosgraph_msgs/msg/Clock@gz.msgs.Clock'],
+        output='screen'
+    )
+    ld.add_action(bridge)
+
+    static_front_laser = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='static_front_laser',
+        arguments=['0', '0.0', '0', '0', '0.0', '0.0', 'map', 'odom'],
+        output='screen'
+    )
+    ld.add_action(static_front_laser)
+
     return ld
