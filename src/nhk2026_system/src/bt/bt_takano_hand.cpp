7fbdd7cb766fb6725016bd54d41e93dfb24d230d
aky <akiyugata271@gmail.com>
2026-04-14 02:37:44 +0000

    TakanoHandActionクラスにprovidedPortsメソッドを実装し、手を開くかどうかの入力ポートを追加

--- src/nhk2026_system/src/bt/bt_takano_hand.cpp
+++ src/nhk2026_system/src/bt/bt_takano_hand.cpp
@@ -1,1 +1,8 @@
 #include "bt/bt_takano_hand.hpp"
+
+BT::PortsList TakanoHandAction::providedPorts()
+{
+  return providedBasicPorts({
+    BT::InputPort<int>("step", false, "Whether to open the hand")
+  });
+}
