2625dee14c828f30ecf275fe6b8379b365095174
aky <akiyugata271@gmail.com>
2026-04-19 09:47:11 +0000

    ResetMclクラスを追加し、位置情報を設定する機能を実装

---
+++ src/nhk2026_system/src/bt/bt_reset_mcl.cpp
@@ -0,0 +1,30 @@
+#include "bt/bt_reset_mcl.hpp"
+
+BT::PortsList ResetMcl::providedPorts()
+{
+    return providedBasicPorts({
+        BT::InputPort<double>("x", 0.0, "initial x position"),
+        BT::InputPort<double>("y", 0.0, "initial y position"),
+        BT::InputPort<double>("theta", 0.0, "initial orientation (radians)")
+    });
+}
+
+bool ResetMcl::setMessage(geometry_msgs::msg::Pose & msg)
+{
+    auto x = getInput<double>("x");
+    auto y = getInput<double>("y");
+    auto theta = getInput<double>("theta");
+
+    if (!x) return false;
+    if (!y) return false;
+    if (!theta) return false;
+
+    msg.position.x = x.value();
+    msg.position.y = y.value();
+    msg.position.z = 0.0;
+    tf2::Quaternion q;
+    q.setRPY(0, 0, theta.value());
+    q.normalize();
+    msg.orientation = tf2::toMsg(q);
+    return true;
+}
