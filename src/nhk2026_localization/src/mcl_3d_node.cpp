408f10e606728f51041f3fd87f58374abc875849
grafen1202 <grafen1202>
2026-04-21 21:18:17 +0900

    filter change

--- src/nhk2026_localization/src/mcl_3d_node.cpp
+++ src/nhk2026_localization/src/mcl_3d_node.cpp
@@ -414,22 +414,22 @@ namespace mcl {
                     xyz2uvw(x_map, y_map, z_map, &u, &v, &w);
 
                     // マップ範囲内かチェック
-                    if (u >= 0 && u < static_cast<int>(dim_x_) && 
-                        v >= 0 && v < static_cast<int>(dim_y_) && 
-                        w >= 0 && w < static_cast<int>(dim_z_)) {
+                    // if (u >= 0 && u < static_cast<int>(dim_x_) && 
+                    //     v >= 0 && v < static_cast<int>(dim_y_) && 
+                    //     w >= 0 && w < static_cast<int>(dim_z_)) {
                         
-                        float sdf_val = distField3D_[getIdx3D(u, v, w)];
+                    //     float sdf_val = distField3D_[getIdx3D(u, v, w)];
                         
-                        // 閾値の設定（例：10cm以上壁から離れていたら無効化）
-                        // 自己位置推定に使用する「確かな壁の点」だけを残す
-                        const float sdf_threshold = 0.10f; 
-                        if (std::abs(sdf_val) > sdf_threshold) {
-                            continue; // 壁から遠い点（動的障害物やノイズ）なので除外
-                        }
-                    } else {
-                        // マップ外の点は、静止物体の情報がないため除外（または必要に応じて保持）
-                        continue;
-                    }
+                    //     // 閾値の設定（例：10cm以上壁から離れていたら無効化）
+                    //     // 自己位置推定に使用する「確かな壁の点」だけを残す
+                    //     const float sdf_threshold = 0.10f; 
+                    //     if (std::abs(sdf_val) > sdf_threshold) {
+                    //         continue; // 壁から遠い点（動的障害物やノイズ）なので除外
+                    //     }
+                    // } else {
+                    //     // マップ外の点は、静止物体の情報がないため除外（または必要に応じて保持）
+                    //     continue;
+                    // }
 
                    
 
